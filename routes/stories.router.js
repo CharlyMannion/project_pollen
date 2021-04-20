bce80986a2febb161d231aea3632d48925a63c7a
Edwin Simpson <edwinsimpson@edwins-mbp-2.home>
2021-04-20 13:53:39 +0100

    added missing module-exports for stories.router

--- routes/stories.router.js
+++ routes/stories.router.js
@@ -3,3 +3,5 @@ const storiesRouter = require('express').Router();
 storiesRouter.route('/stories').get(getStories);
 
 storiesRouter.route('/stories/:id').get(getStory);
+
+module.exports = storiesRouter;
