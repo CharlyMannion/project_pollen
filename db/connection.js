0cff4e5486039a6daa06da09acd9a033e60ec43e
Edwin Simpson <edwinsimpson@edwins-mbp-2.home>
2021-04-20 10:51:14 +0100

    add mongodb env in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/project_pollen", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/project_pollen", {
     useNewUrlParser: true,
     useUnifiedTypology: true,
     useFindAndModify: false,
-    useCreateIndex, true
+    useCreateIndex: true
 });
