32ad82c7adbaff9cca3b0252319c8d04c074c655
Edwin Simpson <edwinsimpson@edwins-mbp-2.home>
2021-04-20 16:47:38 +0100

    add get route for landing page

--- routes/api.router.js
+++ routes/api.router.js
@@ -3,4 +3,13 @@ const storyRouter = require('./stories.router');
 
 router.use('/stories', storyRouter);
 
+router.get('/', (req, res) => {
+    res.json({
+        'possible endpoints:': {
+            '/api/stories': ['GET'],
+            '/api/storiess/:id': ['GET']
+        }
+    });
+});
+
 module.exports = router;
