2e5f0686ff6bd220ee50422f02478bd494a648dc
Edwin Simpson <edwinsimpson@edwins-mbp-2.home>
2021-04-21 10:15:03 +0100

    fix story routes

--- routes/stories.router.js
+++ routes/stories.router.js
@@ -1,8 +1,8 @@
 const storiesRouter = require('express').Router();
 const { getStory, getStories} = require('../controllers/stories.controller');
 
-storiesRouter.route('/stories').get(getStories);
+storiesRouter.route('/').get(getStories);
 
-storiesRouter.route('/stories/:id').get(getStory);
+storiesRouter.route('/:id').get(getStory);
 
 module.exports = storiesRouter;
